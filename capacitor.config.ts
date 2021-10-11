29398777fcb4c774b029122a2d34de5cc3e21542
srphihung <sr.phihung@gmail.com>
2021-10-11 11:54:13 +0200

    APK

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dieet.app',
+  appName: '1-1Diet',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
