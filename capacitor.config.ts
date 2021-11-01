f6ff5c1361e023f4a9cbbe4b98e2b1f657b82ec7
timrebergen <tim.t.rebergen@student.hu.nl>
2021-11-01 11:07:41 +0100

    Changes_01_11_2021
    Changes_01_11_2021

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,13 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+<<<<<<< Updated upstream
   appId: 'com.dieet.app',
   appName: '1-1Diet',
+=======
+  appId: 'com.diet.app',
+  appName: '1op1Dieet',
+>>>>>>> Stashed changes
   webDir: 'build',
   bundledWebRuntime: false
 };
