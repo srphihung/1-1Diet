b15072128d9f77d7259036799d8820a912e3e484
timrebergen <tim.t.rebergen@student.hu.nl>
2021-11-09 12:36:57 +0100

    Log-in CSS styling 12:36 - 08-11-21
    Aanpassingen aan de log-in styling en lay-out.

--- src/pages/js/Login/Login.js
+++ src/pages/js/Login/Login.js
@@ -1,4 +1,4 @@
-import { IonButton, IonContent, IonInput, IonItem } from '@ionic/react';
+import {IonButton, IonContent, IonInput, IonItem} from '@ionic/react';
 import Logo from "../../../images/logodiet.svg";
 import '../../styles/Login/Login.css'
 import React, {useState} from "react";
@@ -11,31 +11,32 @@ function Login() {
         <IonContent>
             <div>
                 <div className="navigationInfo">
-                    <img className="logo" src={Logo}  alt={'1:1 Diet logo'}/>
+                    <img className="logo" src={Logo} alt={'1:1 Diet logo'}/>
                 </div>
                 <form action={"./home"}>
-                <div className="loginContainer">
-                    <h2>Login</h2>
-                    <div className="loginUsername">
-                        <IonItem>
-                            <IonInput name={"username"} value={username} placeholder="Gebruikersnaam" required={"true"}/>
-                        </IonItem>
-                    </div>
-                    <div className="loginPassword">
-                        <IonItem>
-                            <IonInput name={"password"} type="password" value={password} minlength="8" placeholder="Wachtwoord" required={"true"}/>
-                        </IonItem>
-                    </div>
-                    <div className="loginSubmit">
-                            <IonButton type="submit" color="none">
+                    <div className="loginContainer">
+                        <h2>Login</h2>
+                        <div className="userInput" id="loginUsername">
+                            <IonItem>
+                                <IonInput name={"username"} value={username} placeholder="Gebruikersnaam"
+                                          required={"true"}/>
+                            </IonItem>
+                        </div>
+
+                        <div className="userInput" id="loginPassword">
+                            <IonItem>
+                                <IonInput name={"password"} type="password" value={password} minlength="8"
+                                          placeholder="Wachtwoord" required={"true"}/>
+                            </IonItem>
+                        </div>
+
+                            <IonButton className="userInput" id="loginSubmit" type="submit" color="none">
                                 Login
                             </IonButton>
                     </div>
-                </div>
                 </form>
             </div>
         </IonContent>
     )
 }
-
 export default Login
