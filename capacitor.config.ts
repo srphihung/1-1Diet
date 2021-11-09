a6d93010149c8abcae13375fd376d7ad96013a21
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-11-09 11:40:02 +0100

    Login v1.12

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-<<<<<<< Updated upstream
-  appId: 'com.dieet.app',
-  appName: '1-1Diet',
-=======
   appId: 'com.diet.app',
   appName: '1op1Dieet',
->>>>>>> Stashed changes
   webDir: 'build',
   bundledWebRuntime: false
 };
--- src/pages/js/Login/Login.js
+++ src/pages/js/Login/Login.js
@@ -1,87 +1,11 @@
-import {
-    IonAvatar,
-    IonButton,
-    IonCard,
-    IonCardContent,
-    IonContent,
-    IonIcon,
-    IonInput,
-    IonItem,
-    IonText
-} from '@ionic/react';
+import { IonButton, IonContent, IonInput, IonItem } from '@ionic/react';
 import Logo from "../../../images/logodiet.svg";
 import '../../styles/Login/Login.css'
 import React, {useState} from "react";
-import { Link } from 'react-router-dom'
 
 function Login() {
     const [username] = useState("")
     const [password] = useState("")
-    class loginForm extends React.Component {
-        constructor() {
-            super();
-            this.state = {
-                input: {},
-                errors: {}
-            };
-
-            this.handleChange = this.handleChange.bind(this);
-            this.handleSubmit = this.handleSubmit.bind(this);
-        }
-
-        handleChange(login) {
-            let input = this.state.input;
-            input[login.target.name] = login.target.value;
-
-            this.setState({
-                input
-            });
-        }
-
-        handleSubmit(login) {
-            login.preventDefault();
-
-            if (this.validate()) {
-                console.log(this.state);
-
-                let input = {};
-                input["username"] = "";
-                input["password"] = "";
-                this.setState({input: input});
-
-                alert('Access Granted');
-            }
-        }
-
-        validate() {
-            let input = this.state.input;
-            let errors = {};
-            let isValid = true;
-
-            if (!input["username"]) {
-                isValid = false;
-                errors["username"] = "Please enter your name.";
-            }
-            if (!input["password"]) {
-                isValid = false;
-                errors["password"] = "Please enter your password.";
-            }
-
-            if (typeof input["password"] !== "undefined") {
-
-                if (input["password"] < 8) {
-                    isValid = false;
-                    errors["password"] = "Passwords to short.";
-                }
-            }
-
-            this.setState({
-                errors: errors
-            });
-
-            return isValid;
-        }
-    }
 
     return (
         <IonContent>
@@ -94,12 +18,12 @@ function Login() {
                     <h2>Login</h2>
                     <div className="loginUsername">
                         <IonItem>
-                            <IonInput value={username} placeholder="Gebruikersnaam" required/>
+                            <IonInput name={"username"} value={username} placeholder="Gebruikersnaam" required={"true"}/>
                         </IonItem>
                     </div>
                     <div className="loginPassword">
                         <IonItem>
-                            <IonInput type="password" value={password} minlength="8" placeholder="Wachtwoord" required/>
+                            <IonInput name={"password"} type="password" value={password} minlength="8" placeholder="Wachtwoord" required={"true"}/>
                         </IonItem>
                     </div>
                     <div className="loginSubmit">
