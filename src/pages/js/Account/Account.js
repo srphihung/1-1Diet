6ab0800ff333ba2c0b2b8e731df01de0d7311059
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-11-10 11:29:18 +0100

    Account v1

--- src/pages/js/Account/Account.js
+++ src/pages/js/Account/Account.js
@@ -2,12 +2,22 @@ import React, { useEffect, useState } from 'react'
 import TopNav from '../TopNav'
 import '../../styles/Account/Account.css'
 import { scaleOutline, resizeOutline, alertCircleOutline, settingsOutline, chevronForwardOutline, person } from 'ionicons/icons'
-import { IonAvatar, IonButton, IonCard, IonCardContent, IonContent, IonIcon, IonText } from '@ionic/react'
-import CurrentUser from '../../../Data/CurrentUser.json'
+import {
+    IonAvatar,
+    IonButton,
+    IonCard,
+    IonCardContent,
+    IonContent,
+    IonIcon,
+    IonImg,
+    IonItem,
+    IonText
+} from '@ionic/react'
+import DF from '../../../Data/DF.json'
 import { Link } from 'react-router-dom'
 
 function Account() {
-    const [currentUser, setCurrentUer] = useState(CurrentUser[0])
+    const [currentUser, setCurrentUer] = useState(DF[0])
     const [loading, setLoading] = useState(true)
 
     var today = new Date();
@@ -22,29 +32,28 @@ function Account() {
             <div>
                 <TopNav />
                 <div className="accountPageContent">
-                    <button className="settingsBtn"><IonIcon icon={settingsOutline} className="iconProfile"></IonIcon></button>
-                    <IonAvatar className="Avatar"><IonIcon icon={person} className="avatarIcon"></IonIcon></IonAvatar>
-                    <p>Welkom {currentUser.user_firstname} {currentUser.user_lastname} </p>
+                    <button className="settingsBtn">
+                        <IonIcon icon={settingsOutline} className="iconProfile">
+                        </IonIcon>
+                    </button>
+                    <IonAvatar className="profileImg">
+                        <IonImg src={DF.urlClientPhoto}/>
+                    </IonAvatar>
+                    <p>Welkom {DF.firstName} {DF.lastName} </p>
                     <p>{today}</p>
                     <hr />
                     <div className="statContainer">
-                        <div className="heightContainer"><div className="blueborder"><IonIcon icon={resizeOutline} className="iconProfile"></IonIcon></div><p>{currentUser.user_height} Cm</p></div>
-                        <div className="weightContainer"><div className="blueborder"><IonIcon icon={scaleOutline} className="iconProfile"></IonIcon></div><p>{currentUser.user_currentweight} Kg</p></div>
+                        <div className="heightContainer"><div className="blueborder"><IonIcon icon={resizeOutline} className="iconProfile"></IonIcon></div><p>{DF.lengthInCm} Cm</p></div>
+                        <div className="weightContainer"><div className="blueborder"><IonIcon icon={scaleOutline} className="iconProfile"></IonIcon></div><p>{DF.startWeight} Kg</p></div>
                     </div>
                     <div className="consulentContainer">
                         <h3>Aangesloten bij</h3>
-                        {currentUser.consulent.length === 1 ? (
                             <div className="consulentShrtct">
-                                <IonAvatar className="consulentAvatar">
-                                    <img src={currentUser.consulent[0].consulent_image} className="consulentProfileImg"></img>
-                                </IonAvatar>
                                 <div className="consulentContent">
-                                    <p>{currentUser.consulent[0].consulent_name}</p>
-                                    <p>{currentUser.consulent[0].consulent_adres}</p>
+                                    <p>{DF.consultantFullName}</p>
                                 </div>
                                 <IonIcon icon={chevronForwardOutline} className="iconConsulent"></IonIcon>
                             </div>
-                        ) : (
                             <div className="noConsulentWarning">
                                 <div className="blueWarning">
                                     <IonIcon icon={alertCircleOutline} className="iconProfile"></IonIcon>
@@ -54,21 +63,12 @@ function Account() {
                                     <button className="searchConsulent">Consulent zoeken</button>
                                 </div>
                             </div>
-                        )}
                     </div>
                     {/* {currentUser.user_id === Number  ? ( */}
                     <h3>Laatste stats</h3>
                     <div className="IonCardStats">
                         <IonCard className="userStatContainer">
                             <IonCardContent className="userStatContainerContent">
-                                {currentUser.user_appointments.slice(0,2).map((appointment) => {
-                                    return <IonCardContent key={appointment.title}>
-                                        <IonText className="statTitle">{appointment.title}</IonText><br />
-                                        <IonText className="statDate">{appointment.date}</IonText>
-                                        <div className="weightMomentCircle">{appointment.weight} Kg</div>
-                                        <hr />
-                                    </IonCardContent>
-                                })}
                                 <Link to="/voortgang"><IonButton className="toProgress" shape="round">Volledig rapport</IonButton></Link>
                             </IonCardContent>
                         </IonCard>
