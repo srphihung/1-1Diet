aa58cf6ca0031e81d053412ea1692f99f55cb20c
timrebergen <60223267+timrebergen@users.noreply.github.com>
2021-11-18 14:00:25 +0100

    Updated_axios_get_post_use_effect

--- src/pages/js/Account/Account.js
+++ src/pages/js/Account/Account.js
@@ -1,4 +1,5 @@
 import React, {Component, useEffect, useState} from 'react'
+
 import TopNav from '../TopNav'
 import '../../styles/Account/Account.css'
 import { scaleOutline, resizeOutline, alertCircleOutline, settingsOutline, chevronForwardOutline, person, locationOutline } from 'ionicons/icons'
@@ -18,52 +19,130 @@ import DF from '../../../Data/DF.json'
 import { Link } from 'react-router-dom'
 import Logo from "../../../images/logodiet.svg";
 import * as QueryString from "querystring";
+import axios from "axios";
 
 function Account(props) {
+    useEffect(() => {getAuthentication()}, []);
     const [currentUser, setCurrentUer] = useState(DF[0])
     const [loading, setLoading] = useState(true)
-    const [showModal, setShowModal] = useState(false)
-
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    today = dd + '/' + mm + '/' + yyyy;
-
+    const [showModal, setShowModal] = useState(false);
     const axios = require("axios")
-    axios.post('https://test-api-cwp.vp-company.nl/connect/token', QueryString.stringify({
-        username: "rcwtest1@1op1dieet.nl",
-        password: "Onzin&21&",
-        grant_type: "password",
-    }), {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-        }
-    }).then(response => {
-        console.log(response.data)
-    }).catch(err => console.log(err.response))
 
-    const instance = axios.create({
-        baseURL: 'https://test-api-cwp.vp-company.nl/api/',
-        method: 'GET',
-        timeout: 1000,
-        headers: {'Authorization': 'Bearer '+"eyJhbGciOiJSUzI1NiIsImtpZCI6Ijg1MTdCRDk3NjZBQ0M5Qzg5OERCRUZFM0Y4MjI2MjA1QTU5RUQ4QjUiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiIxNjg1NCIsInJjd3VpZCI6IjE2ODU0IiwidG9rZW5fdXNhZ2UiOiJhY2Nlc3NfdG9rZW4iLCJqdGkiOiJmODBiODQwNS02NWZhLTQ0NDQtYjg2Zi00YjdiYjkxMDA1MzgiLCJzY29wZSI6WyJwcm9maWxlIiwib2ZmbGluZV9hY2Nlc3MiXSwibmJmIjoxNjM2ODAzNjYwLCJleHAiOjE2MzY4MDcyNjAsImlhdCI6MTYzNjgwMzY2MCwiaXNzIjoiaHR0cHM6Ly90ZXN0LWFwaS1jd3AudnAtY29tcGFueS5ubC8ifQ.cVwB2PEVjJbiscM_AVeK_e0xg3MV5HLzOngS4T-S84Gk8xi2GrJDkcLM2DBp0AgpORfg06ob8mNYKQmPyaQYgGiHDK5YvRbvv8Vf2PO_CHNsf81IR7SVW4L83IGUOGvZMMXd5gJfhddWZcBS7piuQwOMVCnSAW7xLDXnE3IJI_5pbquDewCf_phPdCYSq6XEjrSFJkbGVw2wIPJAGlovX6ix7Ki-axQvZK26UeUo29xqtUJuElayBqHyv5HAHDe586m3HFAxDjFqiMBYpiwRtpAPKUe2yxlShbO0c-jHUEy-Mys1g9qzkd6G2QZbWd_5a32WC_8SqxEOA4X70PHKJnYbyN_6vhDgogtpL5-LrTCEriJCB8pLPj-RoP66KWehX8HYb-T8hRwS57MF4b2zm1yj2BcWvnUPqUau_Qm-7gF2opxXlLinNNtEn7jqdE0dfadWDPrJHhdfQJcEXYS-K1jxApKgvNrKStJZOTyvgNZ7MIKhv9cexwU7eX-rSthXdWtEj71hNPJtrqqgJdS8dCiDgItXJ7ZeLtTXc-It32hcCgjXMcu08hDwCsifoJ2mDNXOeQFHELM-N8AHc7ySBZfsqFqY1rOL5WJyFFkAf9Thp8gpvnbv3Y6XPt_w0WMNm95124CgVvfk4BASgQVEOna6hCgTR_tiXl0uVFA6lDo"}
-    });
-
-    fetch('https://test-api-cwp.vp-company.nl/api/')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
-        .catch(error => console.error(error));
+    const getAuthentication = ( ) => {
+        axios.post('https://test-api-cwp.vp-company.nl/connect/token', QueryString.stringify({
+            username: "rcwtest1@1op1dieet.nl",
+            password: "Onzin&21&",
+            grant_type: "password",
+        }), {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            }
+        }).then(response => {
+            console.log(response.data)
+        }).catch(err => console.log(err.response))
+    }
+    
 
 
-    instance.get('/client/accountinformation')
-        .then(response => {
-            return response.data;
-        })
 
+    // const axiosInstance = axios.create({
+    //     baseURL: 'https://test-api-cwp.vp-company.nl/api/',
+    //     method: 'GET',
+    //     timeout: 1000,
+    //     headers: {
+    //         'Authorization': localStorage.getItem('access_token') ? "JWT " + localStorage.getItem('access_token') : null,
+    //         'Content-Type': 'application/x-www-form-urlencoded',
+    //         'accept': 'application/x-www-form-urlencoded'
+    //     }
+    // });
+    // axiosInstance.interceptors.response.use(
+    //     response => response,
+    //     error => {
+    //         const originalRequest = error.config;
+    //
+    //         // Prevent infinite loops
+    //         if (error.response.status === 401 && originalRequest.url === 'https://test-api-cwp.vp-company.nl/api/'+'token/refresh/') {
+    //             window.location.href = '/account/';
+    //             return Promise.reject(error);
+    //         }
+    //
+    //         if (error.response.data.code === "token_not_valid" &&
+    //             error.response.status === 401 &&
+    //             error.response.statusText === "Unauthorized")
+    //         {
+    //             const refreshToken = localStorage.getItem('refresh_token');
+    //
+    //             if (refreshToken){
+    //                 const tokenParts = JSON.parse(atob(refreshToken.split('.')[1]));
+    //
+    //                 // exp date in token is expressed in seconds, while now() returns milliseconds:
+    //                 const now = Math.ceil(Date.now() / 1000);
+    //                 console.log(tokenParts.exp);
+    //
+    //                 if (tokenParts.exp > now) {
+    //                     return axiosInstance
+    //                         .post('/token/refresh/', {refresh: refreshToken})
+    //                         .then((response) => {
+    //
+    //                             localStorage.setItem('access_token', response.data.access);
+    //                             localStorage.setItem('refresh_token', response.data.refresh);
+    //
+    //                             axiosInstance.defaults.headers['Authorization'] = "JWT " + response.data.access;
+    //                             originalRequest.headers['Authorization'] = "JWT " + response.data.access;
+    //
+    //                             return axiosInstance(originalRequest);
+    //                         })
+    //                         .catch(err => {
+    //                             console.log(err)
+    //                         });
+    //                 }else{
+    //                     console.log("Refresh token is expired", tokenParts.exp, now);
+    //                     window.location.href = '/client/';
+    //                 }
+    //             }else{
+    //                 console.log("Refresh token not available.")
+    //                 window.location.href = '/client/';
+    //             }
+    //         }
+    //
+    //
+    //         // specific error handling done elsewhere
+    //         return Promise.reject(error);
+    //     }
+    // );
+    //
+    // (async () => {
+    //     axios.interceptors.request.use(
+    //         function (req) {
+    //             req.time = { startTime: new Date() };
+    //             return req;
+    //         },
+    //         (err) => {
+    //             return Promise.reject(err);
+    //         }
+    //     );
+    //
+    //     axios.interceptors.response.use(
+    //         function (res) {
+    //             res.config.time.endTime = new Date();
+    //             res.duration =
+    //                 res.config.time.endTime - res.config.time.startTime;
+    //             return res;
+    //         },
+    //         (err) => {
+    //             return Promise.reject(err);
+    //         }
+    //     );
+
+        // axios
+        //     .get("http://localhost:8100/account")
+        //     .then((res) => {
+        //         console.log(res.duration)
+        //     })
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+    // })();
     return (
         <IonContent>
                 <TopNav/>
@@ -94,10 +173,12 @@ function Account(props) {
                             <div className="blueborder">
                                 <IonIcon icon={scaleOutline} className="iconProfile"></IonIcon>
                             </div>
-                            
                             <IonModal isOpen={showModal} onDidDismiss={() => setShowModal(false)} swipe-to-close="true" className='WeightModal'>
                                 <div className="modal-dialog modal-dialog-scrollable">
-                                    <h2>Gewichten</h2>
+                                    <div className="navigationInfo">
+                                        <img className="logo" src={Logo} alt={'1:1 Diet logo'}/>
+                                    </div>
+                                    <h2>Gewichten:</h2>
                                     <IonCol className="weightDetails">
                                         <p><strong>Start Gewicht:</strong> {DF.startWeight}</p>
                                         <p><strong>Start BMI:</strong> {DF.startBMI}</p>
@@ -105,22 +186,21 @@ function Account(props) {
                                         <p><strong>Doel Gewicht:</strong> {DF.targetWeight}</p>
                                         <p><strong>Doel BMI:</strong> {DF.targetBMI}</p>
                                         <p><strong>Doel Middel:</strong> {DF.targetWaistSize}</p>
-                                        <p><strong>Vet:</strong> {(DF.clientWeightDetails.showFat === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Middel:</strong> {(DF.clientWeightDetails.showWaist === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Visceraal Vet:</strong> {(DF.clientWeightDetails.showVisceralFat === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Vet Vrij Massa:</strong> {(DF.clientWeightDetails.showFatFreeMass === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Gespierde Massa:</strong> {(DF.clientWeightDetails.showMuscularMass === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Bot Mass:</strong> {(DF.clientWeightDetails.showBoneMass === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>BmrKc:</strong> {(DF.clientWeightDetails.showBmrKc === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>BmrKj:</strong> {(DF.clientWeightDetails.showBmrKj === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Metabloische Leeftijd:</strong> {(DF.clientWeightDetails.showMetabolicAge === 'true') ? "Onwaar" :  "Waar" }</p>
-                                        <p><strong>Gezond Gewicht:</strong> {(DF.clientWeightDetails.showHealthyWeight === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Vet Zien:</strong> {(DF.clientWeightDetails.showFat === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Middel Zien:</strong> {(DF.clientWeightDetails.showWaist === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Visceraal Vet Zien:</strong> {(DF.clientWeightDetails.showVisceralFat === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Vet Vrij Massa Zien:</strong> {(DF.clientWeightDetails.showFatFreeMass === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Gespierde Massa Zien:</strong> {(DF.clientWeightDetails.showMuscularMass === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Bot Massa Zien:</strong> {(DF.clientWeightDetails.showBoneMass === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat BmrKc zien:</strong> {(DF.clientWeightDetails.showBmrKc === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat BmrKj zien:</strong> {(DF.clientWeightDetails.showBmrKj === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Metabloische Leeftijd Zien:</strong> {(DF.clientWeightDetails.showMetabolicAge === 'true') ? "Onwaar" :  "Waar" }</p>
+                                        <p><strong>Laat Gezond Gewicht Zien:</strong> {(DF.clientWeightDetails.showHealthyWeight === 'true') ? "Onwaar" :  "Waar" }</p>
                                     </IonCol>
-                                    
+                                    <IonButton className="WeightModalCloseButton" color="none" onClick={() => setShowModal(false)}>Close Modal</IonButton>
                                 </div>
-                                <IonButton className="WeightModalCloseButton" color="none" onClick={() => setShowModal(false)}>Close Modal</IonButton>
-                            </IonModal>
 
+                            </IonModal>
                             <p>
                                 <IonButton className="WeightModalButton" onClick={() => setShowModal(true)} color="none">Gewicht</IonButton>
                             </p>
