a9c9b6fb2f6d818048b67953744951a624e1e152
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-11-25 11:35:31 +0100

    Revert "login auth v1.34"
    This reverts commit 3caf5ea9

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,6 +1,6 @@
 export default interface IUser {
     id?: any | null,
-    username?: string | null,
+    username?: string,
     password?: string,
     roles?: Array<string>,
     clientId?: number,
