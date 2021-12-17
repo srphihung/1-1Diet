a31a1d5caa152f6c2fc8076a76f521e8f70e8d23
lucius02 <54938206+lucius02@users.noreply.github.com>
2021-12-17 09:49:22 +0100

    Revert "styling css v1.45"
    This reverts commit 4366de12

--- src/components/scrollNavProfile.tsx
+++ src/components/scrollNavProfile.tsx
@@ -4,29 +4,18 @@ import UserService from "../services/user.service"
 import '../pages/styles/Profile/Styles.css'
 import {Redirect} from "react-router-dom";
 import {
-    IonAvatar,
     IonContent,
-    IonIcon,
-    IonImg,
     IonSlides,
     IonSlide,
     IonItem,
     IonCol,
     IonRow,
-    IonGrid,
-    IonHeader
+    IonGrid
 } from "@ionic/react";
-import {alertCircleOutline, chevronForwardOutline, resizeOutline} from "ionicons/icons";
-import TopNav from "../pages/js/TopNav";
-import AboutImg1 from "../images/aboutimg.svg";
-import AboutImg2 from "../images/aboutimg2.svg";
 import maleLength from "../images/maleLength.png";
-import OrangeFrame from "../images/OrangeFrame.png"
-import Sidebar from './Sidebar';
+import OrangeFrame from "../images/OrangeFrame.png";
 import ChartStatsWeightComponent from "./chartStatsWeight.component";
 import ChartStatsBMIComponent from "./chartStatsBMI.component";
-import VariableWeightMomentsComponent from "./variableWeightMoments.component"
-
 type Props = {};
 
 type State = {
@@ -70,77 +59,74 @@ export default class ScrollNav extends Component<Props, State> {
 
         return (
             <IonContent>
-
                 <IonItem className="userContainer">
-                    <h1 id="userName">{userContent.firstName}'s Home</h1>
+                        <h1 id="userName">{userContent.firstName}'s Home</h1>
                 </IonItem>
-                <IonSlides className="slideContainer">
-                    <IonSlide>
-                        <div className="aboutContainer">
-                            <h2 id="startBMI">
-                                Je Gewicht
-                            </h2>
-                            <ChartStatsWeightComponent/>
-                            <IonGrid>
-                                <IonRow className="BMITextContainer">
-                                    <IonCol size="4">{userContent.startBMI}</IonCol>
-                                    <IonCol>{userContent.targetBMI} </IonCol>
-                                    <IonCol size="4">{userContent.startBMI}</IonCol>
+
+                    <IonSlides pager>
+                        <IonSlide>
+                            <div className="aboutContainer">
+                                    <h2 id="startWeight">
+                                        Je Gewicht
+                                        </h2>
+                                    <ChartStatsWeightComponent/>
+                                <IonGrid>
+                                <IonRow className="weightTextContainer" id="weightText">
+                                    <IonCol >
+                                        <p >{userContent.startWeight} kg</p>
+                                        <p>Start Gewicht</p>
+                                    </IonCol>
+                                    <IonCol offset="26">
+                                        <p>90 kg</p>
+                                        <p>Huidig Gewicht</p>
+                                    </IonCol>
+                                    <IonCol offset="50">
+                                        <p>{userContent.targetWeight} kg</p>
+                                        <p>Streef Gewicht</p>
+                                    </IonCol>
                                 </IonRow>
-                            </IonGrid>
-                        </div>
-                    </IonSlide>
+                                </IonGrid>
+                            </div>
+                        </IonSlide>
 
-                    <IonSlide>
-                        <div className="aboutContainer">
-                            <h2 id="startBMI">
-                                Je Gewicht
-                            </h2>
-                            <ChartStatsBMIComponent/>
-                            <IonGrid>
-                                <IonRow className="BMITextContainer">
-                                    <IonCol size="4">{userContent.startBMI}</IonCol>
-                                    <IonCol>{userContent.targetBMI} </IonCol>
-                                    <IonCol size="4">{userContent.startBMI}</IonCol>
-                                </IonRow>
-                            </IonGrid>
-                        </div>
-                    </IonSlide>
+                        <IonSlide>
+                            <div className="aboutContainer">
+                                <h2 id="startWeight">
+                                    Je Gewicht
+                                </h2>
+                                <ChartStatsBMIComponent/>
+                                <IonGrid>
+                                    <IonRow className="weightTextContainer" id="weightText">
+                                        <IonCol >
+                                            <p >{userContent.startBMI}</p>
+                                            <p>Start BMI</p>
+                                        </IonCol>
+                                        <IonCol offset="26">
+                                            <p>27,8</p>
+                                            <p>Huidig BMI</p>
+                                        </IonCol>
+                                        <IonCol offset="50">
+                                            <p>{userContent.targetBMI}</p>
+                                            <p>Doel BMI</p>
+                                        </IonCol>
 
-                    <IonSlide>
-                        <div className="aboutContainer">
-                            <p id="userHeight">
-                                <p>Je lengte</p>
-                                {userContent.lengthInCm} CM
-                            </p>
-                            <img src={OrangeFrame} id="frameImg"></img>
-                            <img src={maleLength} id="maleImg"></img>
-                        </div>
-                    </IonSlide>
+                                    </IonRow>
+                                </IonGrid>
+                            </div>
+                        </IonSlide>
 
-                    <IonSlide>
-                        <div className="aboutContainer">
-                            <p id="Spline">
-                                <p>Laatste weegmomenten</p>
-                                {userContent.lengthInCm} CM
-                            </p>
-                            <VariableWeightMomentsComponent/>
-                            <p> Sinds {userContent.firstWeightmomentDate} t/m {userContent.lastWeightmomentDate} ben je
-                                in totaal 28kg afgevallen</p>
-                        </div>
-                    </IonSlide>
-
-                    <IonSlide>
-                        <div className="row header">
-                            <div className="col">Datum</div>
-
-                        </div>
-                        <div className="row header">
-                            <div className="col">Gewicht</div>
-                        </div>
-                    </IonSlide>
-                </IonSlides>
+                        <IonSlide>
+                            <div className="aboutContainer" >
+                                <div id="userHeight">
+                                    <p>Je lengte</p>
+                                    <p>{userContent.lengthInCm} CM</p>
+                                </div>
+                                <img src={OrangeFrame} id="frameImg"></img>
+                                <img src={maleLength} id="maleImg"></img>
+                            </div>
+                        </IonSlide>
+                    </IonSlides>
             </IonContent>
-        );
+        )
     }
 }
